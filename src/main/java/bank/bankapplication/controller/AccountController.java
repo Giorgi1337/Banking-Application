4e8ed33292f60dabe0f234b69cbd196c358164e4
Giorgi1337 <kvacabaya02@gmail.com>
2024-07-29 12:20:10 +0400

    implement creation of account and view all created accounts

--- src/main/java/bank/bankapplication/controller/AccountController.java
+++ src/main/java/bank/bankapplication/controller/AccountController.java
@@ -1,8 +1,16 @@
 package bank.bankapplication.controller;
 
+import bank.bankapplication.model.Account;
 import bank.bankapplication.service.AccountService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -10,4 +18,25 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @GetMapping("/accounts")
+    public String viewAllAccounts(Model model) {
+        List<Account> accounts = accountService.getAllAccounts();
+        model.addAttribute("accounts", accounts);
+        return "viewAccounts";
+    }
+
+    @GetMapping("/create")
+    public String showCreateAccountForm(Model model) {
+        model.addAttribute("account", new Account());
+        return "createAccount";
+    }
+
+    @PostMapping("/create")
+    public String createAccount(@ModelAttribute Account account, Model model) {
+        accountService.createAccount(
+                account.getAccountNumber(),
+                account.getAccountHolderName(),
+                account.getBalance());
+        return "redirect:/accounts";
+    }
 }
--- src/main/java/bank/bankapplication/service/AccountService.java
+++ src/main/java/bank/bankapplication/service/AccountService.java
@@ -1,14 +1,29 @@
 package bank.bankapplication.service;
 
+import bank.bankapplication.model.Account;
 import bank.bankapplication.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
     private final AccountRepository accountRepository;
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+
+    public Account createAccount(String accountNumber, String accountHolderName, double initialDeposit) {
+        Account account = new Account();
+        account.setAccountNumber(accountNumber);
+        account.setAccountHolderName(accountHolderName);
+        account.setBalance(initialDeposit);
+        return accountRepository.save(account);
+    }
 
 
 }
